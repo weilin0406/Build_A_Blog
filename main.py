e1a4b21a4d66d83b3cec1a3c6a89ea033cabe01e
Weilin Shen <shen800406@gmail.com>
2019-01-05 15:03:35 -0500

    final

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:123456@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://Build-A-Blog:123456@localhost:8889/Build-A-Blog'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
@@ -12,24 +12,24 @@ class Blog(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
-    post = db.Column(db.Boolean)
+    post = db.Column(db.Text)
 
     def __init__(self, title, post):
         self.title = title
         self.post = post 
 
 
-@app.route('/', method="POST")
+@app.route('/', methods=["POST", "GET"])
 def show_blog():
     post_id = request.args.get('id')
-    if (post_id):
+    if post_id:
         blog = Blog.query.get(post_id)
-        return render_template('single_post.html', blog=blog)
+        return render_template('single_post.html', blog=blog, title="Single Post")
     else:
         
         all_blog_posts = Blog.query.all()
         
-        return render_template('blog.html', posts=all_blog_posts)
+        return render_template('blog.html', all_blog_posts=all_blog_posts, title="All Blog")
 
 @app.route('/newpost', methods=['GET', 'POST'])
 def add_entry():
@@ -43,20 +43,20 @@ def add_entry():
         post_entry = request.form['blog_post']
         post_new = Blog(post_title, post_entry)
 
-        if empty_val(post_title) and empty_val(post_entry):
+        if post_title and post_entry:
             db.session.add(post_new)
             db.session.commit()
-            post_link = "/blog?id=" + str(post_new.id)
+            post_link = "/?id=" + str(post_new.id)
             return redirect(post_link)
         else:
-            if not empty_val(post_title) and not empty_val(post_entry):
+            if not post_title and not post_entry:
                 title_error = "Please enter text for blog title"
                 blog_entry_error = "Please enter text for blog entry"
                 return render_template('new_post.html', blog_entry_error=blog_entry_error, title_error=title_error)
-            elif not empty_val(post_title):
+            elif not post_title:
                 title_error = "Please enter text for blog title"
                 return render_template('new_post.html', title_error=title_error, post_entry=post_entry)
-            elif not empty_val(post_entry):
+            elif not post_entry:
                 blog_entry_error = "Please enter text for blog entry"
                 return render_template('new_post.html', blog_entry_error=blog_entry_error, post_title=post_title)
 
